1c9b4fed9b767e67bfcecde10fb2fcfeabea2dd6
j4k3ng <41842937+j4k3ng@users.noreply.github.com>
2022-04-11 14:21:00 +0100

    date input works

--- src/components/Data.js
+++ src/components/Data.js
@@ -10,7 +10,7 @@ export default function Data(props) {
 
  
     const [input, setInput] = React.useState({
-        date:""
+        date: new Date()
     })
 
     function handleChange(event) {
@@ -38,7 +38,20 @@ export default function Data(props) {
     //         })
 
     // }
-
+    function trigger(){
+        console.log(new Date(input.date))
+        let result = new Date(input.date);
+        result.setDate(result.getDate() + 7);
+        console.log(result)
+        // console.log(input.date)
+        // let day = new Date(1993,2,3)
+        // day.setDate(3)
+        // console.log(day.getDate())
+        // let numberOfDaysToAdd = 6;
+        // // let result = input.date.setDate(input.date.getDate() + numberOfDaysToAdd);
+        // let result = day.getDate()
+        // console.log(new Date(result))
+    }
     return (
         <div>
             <h2>Enter the starting date</h2>
@@ -50,11 +63,20 @@ export default function Data(props) {
                 value = {input.date}
                 onChange={handleChange}
             />
+            <TextField 
+                variant="outlined" 
+                color="secondary" 
+                type="date"
+                name="date" 
+                InputProps={{ readOnly: true }}
+                value = {input.date}
+                onChange={handleChange}
+            />
             <Button 
                 color="primary" 
                 size="large" 
                 variant="contained"
-                onClick={console.log("clicked")}
+                onClick={trigger}
                 >   
                 Select
             </Button>
